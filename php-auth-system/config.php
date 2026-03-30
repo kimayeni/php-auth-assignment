b34081bbacdadcab88dc2e98210cec901735d091
kimayeni <158360260+kimayeni@users.noreply.github.com>
2026-03-30 12:37:48 +0100

    Update config.php

--- php-auth-system/config.php
+++ php-auth-system/config.php
@@ -19,4 +19,21 @@ try {
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
+
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'your_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+define('MIN_USERNAME_LENGTH', 3);
+define('MIN_PASSWORD_LENGTH', 8);
+
+try {
+    $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed");
+}
+
 ?>
